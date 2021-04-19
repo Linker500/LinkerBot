88260713e51601b2de64c735143d800e8c8de10d
linker <github@linkerlabs.net>
2021-04-19 16:06:36 -0400

    Added almost-done dvorak translators

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,54 @@ client.on("ready", () => {
     console.log(`Logged in ${client.user?.tag}`)
 })
 
+const prefix = "~"
 client.on("message", (msg) => {
     if(msg.content == "Linker-Sucks")
     {
         msg.reply(":(")
     }
+
+
+    if(msg.content.startsWith(prefix+"dvorak"))
+    {
+        //TODO: qwerty and dvorak strings aren't perfectly accurate
+        let output = ""
+        let qwerty: string
+        qwerty = "qwertyuiopasdfghjkl;zxcvbnm,.QWERTYUIOPASDFGHJKL:ZXCVBNM<>"
+        let dvorak: string
+        dvorak = "',.pyfgcrlaoeuidhtns;qjkxbmwv\"<>PYFGCRLAOEUIDHTNS:QJKXBMWV"
+
+        for(let i=7; i<msg.content.length; i++)
+        {
+            let index = qwerty.indexOf(msg.content.substring(i,i+1))
+            if(index == -1)
+                output += msg.content.substring(i,i+1)
+            else
+                output += dvorak.substring(index,index+1)
+        }
+
+        msg.reply(output)
+    }
+
+    if(msg.content.startsWith(prefix+"qwerty"))
+    {
+        let output = ""
+        let qwerty: string
+        qwerty = "qwertyuiopasdfghjkl;zxcvbnm,.QWERTYUIOPASDFGHJKL:ZXCVBNM<>"
+        let dvorak: string
+        dvorak = "',.pyfgcrlaoeuidhtns;qjkxbmwv\"<>PYFGCRLAOEUIDHTNS:QJKXBMWV"
+
+        for(let i=7; i<msg.content.length; i++)
+        {
+            let index = dvorak.indexOf(msg.content.substring(i,i+1))
+            if(index == -1)
+                output += msg.content.substring(i,i+1)
+            else
+                output += qwerty.substring(index,index+1)
+        }
+
+        msg.reply(output)
+    }
 })
 
-client.login("ODMzNzc2NTA2NDg4MDk0Nzgy.YH3Q4Q.CFHKf4opsEskIWhg0X-VVbuWPxU")
+client.login("")
